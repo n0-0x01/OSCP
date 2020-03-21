4ce87fc4ec560d11816d1b8c01903eeb2f194bff
kali <patrick.williams022@gmail.com>
2020-03-21 15:07:12 +1300

    A script to enumerate members of Active Directoy groups without administrative privileges.

---
+++ Active Directory/FindNestedGroups.ps1
@@ -0,0 +1,29 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+$PDC = ($domainObj.PdcRoleOwner).Name
+
+$SearchString = "LDAP://"
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+
+$SearchString += $DistinguishedName
+
+$SearchString
+
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+
+$Searcher.SearchRoot = $objDomain
+
+$Searcher.filter="samAccountType=268435456"
+
+$Result =$Searcher.FindAll()
+
+Foreach($obj in $Result)
+{ 
+    if ($obj.Properties.member) {$obj.Properties.name + " has member: " + $obj.Properties.member}
+
+}
