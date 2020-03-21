fed68afde765400d5174a04f8551f7223c30b808
unknown <patrick.williams022@gmail.com>
2020-03-21 18:14:25 +1300

    SPN scripts

---
+++ FindSPN.ps1
@@ -0,0 +1,31 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+$PDC = ($domainObj.PdcRoleOwner).Name
+
+$SearchString = "LDAP://"
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+
+$SearchString += $DistinguishedName
+
+$SearchString
+
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+
+$Searcher.SearchRoot = $objDomain
+
+$Searcher.filter="serviceprincipalname=*"
+
+$Result =$Searcher.FindAll()
+
+Foreach($obj in $Result)
+{ 
+    foreach($prop in $obj.Properties)
+    {
+        $prop 
+    }
+}
